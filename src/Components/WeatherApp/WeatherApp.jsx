6ea1b3e55a6c509de4523c2912f3c0608c720086
Adam <adammiller.2000nj@gmail.com>
2024-04-03 15:07:02 -0400

    assigned variables and mounted elements

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -12,11 +12,34 @@ import humidity_icon from '../Assets/humidity.png';
 
 export default function WeatherApp() {
 
+    let API_KEY = '7be217663430296fbe3c4669f9c20278';
+
+    const search = async () => {
+        const element = document.querySelector(".cityInput");
+        if(element.value === ""){
+            return 0;
+        }
+
+        let URL = `https://api.openweathermap.org/data/2.5/weather?q=${element.value}&units=imperial&appid=${API_KEY}`;
+
+        let resp = await fetch(URL);
+        let data = await resp.json();
+        const humidity = document.querySelector(".humidity-percent");
+        const wind = document.querySelector(".wind-rate");
+        const temp = document.querySelector(".weather-temp");
+        const location = document.querySelector(".weather-location");
+
+        humidity.innerHTML = data.main.humidity + "%";
+        wind.innerHTML = data.wind.speed + "mph";
+        temp.innerHTML = data.main.temp + "ºF";
+        location.innerHTML = data.name;
+    }
+
     return(
         <div className="container">
             <div className="top-bar">
                 <input type="text" className="cityInput" placeholder="Search" />
-                <div className="search-icon">
+                <div className="search-icon" onClick={() => {search()}}>
                     <img src={search_icon} alt="" />
                 </div>
             </div>
@@ -25,7 +48,7 @@ export default function WeatherApp() {
             </div>
             <div className="weather-temp">24ºc</div>
             <div className="weather-location">London</div>
-            <div className="data container">
+            <div className="data-container">
                 <div className="element">
                     <img src={humidity_icon} alt="" className="icon" />
                     <div className="data">
@@ -36,7 +59,7 @@ export default function WeatherApp() {
                 <div className="element">
                     <img src={wind_icon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity-percent">18 km/h</div>
+                        <div className="wind-rate">18 km/h</div>
                         <div className="text">Wind Speed</div>
                     </div>
                 </div>
