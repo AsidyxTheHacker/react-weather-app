519a283adb9e5928986195fb64cfb089f9553db6
Adam <adammiller.2000nj@gmail.com>
2024-04-03 15:32:41 -0400

    added hover and fade-in animations

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './WeatherApp.css';
 
 import search_icon from '../Assets/search.png';
@@ -14,8 +14,11 @@ export default function WeatherApp() {
 
     let API_KEY = '7be217663430296fbe3c4669f9c20278';
 
+    const [wicon, setWicon] = useState(cloud_icon);
+
     const search = async () => {
         const element = document.querySelector(".cityInput");
+        document.querySelector('.weather-info').style.animation = "fadeInAnimation 1s ease";
         if(element.value === ""){
             return 0;
         }
@@ -30,9 +33,31 @@ export default function WeatherApp() {
         const location = document.querySelector(".weather-location");
 
         humidity.innerHTML = data.main.humidity + "%";
-        wind.innerHTML = data.wind.speed + "mph";
-        temp.innerHTML = data.main.temp + "ºF";
+        wind.innerHTML = Math.floor(data.wind.speed) + "mph";
+        temp.innerHTML = Math.floor(data.main.temp) + "ºF";
         location.innerHTML = data.name;
+
+        if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+            setWicon(clear_icon);
+        } else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n") {
+            setWicon(cloud_icon);
+        } else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n") {
+            setWicon(drizzle_icon);
+        } else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n") {
+            setWicon(drizzle_icon);
+        } else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n") {
+            setWicon(rain_icon);
+        } else if(data.weather[0].icon === "10d" || data.weather[0].icon === "10n") {
+            setWicon(rain_icon);
+        } else if(data.weather[0].icon === "13d" || data.weather[0].icon === "13n") {
+            setWicon(snow_icon);
+        } else {
+            setWicon(clear_icon);
+        }
+
+        element.value = "";
+        let removeAnimation = () => document.querySelector('.weather-info').style.animation = "";
+        setTimeout(removeAnimation, 1500)
     }
 
     return(
@@ -43,8 +68,9 @@ export default function WeatherApp() {
                     <img src={search_icon} alt="" />
                 </div>
             </div>
+            <div className="weather-info">
             <div className="weather-image">
-                <img src={cloud_icon} alt="" />
+                <img src={wicon} alt="" />
             </div>
             <div className="weather-temp">24ºc</div>
             <div className="weather-location">London</div>
@@ -65,5 +91,6 @@ export default function WeatherApp() {
                 </div>
             </div>
         </div>
+            </div>
     )
 };
